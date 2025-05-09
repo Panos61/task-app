f6971da629d64529dda2d3834ada390c1c0811bc
Panos Orovas <panagiwtis.orovas@gmail.com>
2025-05-09 23:21:40 +0300

    update cookie config

--- server/src/utils/cookie.ts
+++ server/src/utils/cookie.ts
@@ -6,5 +6,8 @@ export const cookieConfig: CookieOptions = {
   secure: config.NODE_ENV === 'production',
   maxAge: 1000 * 60 * 60 * 24, // 1 day
   sameSite: config.NODE_ENV === 'production' ? 'none' : 'lax',
+  ...(config.NODE_ENV === 'production' && { 
+    domain: '167.235.30.231' 
+  }),
   path: '/',
 };
